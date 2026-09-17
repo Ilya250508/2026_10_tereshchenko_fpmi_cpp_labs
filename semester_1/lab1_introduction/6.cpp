aaa9fd1ff5aa8c8228642fbd9240702620b8ca4a
Ilya250508 <terilya2008@gmail.com>
2026-09-17 15:38:50 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    using std::cout;
+    using std::cin;
+    long long int b, d, a;
+    cout << "Введите начало и конец отрезка ";
+    cin >> a >> b ;
+    cout << "Введите разность прогрессии ";
+    cin >> d;
+
+
+
+    for (long long int i = a; i <= b; i += d)
+    {
+        if (i % 3 == 0)
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
